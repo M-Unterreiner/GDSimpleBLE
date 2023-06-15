b3145e3ecaab27aec7b5055931203eeb77fbd43f
Martin Unterreiner <martin.unterreiner@igniti.de>
2023-06-15 13:40:35 +0200

    Commented BLEAdapater out

--- demo/scripts/Main.gd
+++ demo/scripts/Main.gd
@@ -30,8 +30,9 @@ extends Control
 
 # Init adpater using the first one
 func _ready() -> void:
-	var adapters = BLEAdapter.get_adapters()
-	_ble_adapter.init(adapters[0])
+	#var adapters = BLEAdapter.get_adapters()
+	#_ble_adapter.init(adapters[0])
+	pass
 
 
 # Write one line in the terminal
@@ -41,15 +42,17 @@ func _print_line(prompt : String, data : String) -> void:
 
 
 # Write the peripheral in the terminal
-func _print_peripheral(peripheral : BLEPeripheral, prompt : String, data : String = "") -> void:
-	var is_connectable_string = "[Connectable] " if peripheral.is_connectable else "[Not connectable] "
-	_terminal.write(prompt + ": ", Color.PURPLE)
-	_terminal.write(peripheral.identifier + " " + str(peripheral.rssi) + "dBm " + is_connectable_string + " ")
-	if data != "":
-		_terminal.write(peripheral.address, Color.GREEN)
-		_terminal.writeln(" => " + data, Color.TOMATO)
-	else:
-		_terminal.writeln(peripheral.address, Color.GREEN)
+# func _print_peripheral(peripheral : BLEPeripheral, prompt : String, data : String = "") -> void:
+func _print_peripheral(prompt : String, data : String = "") -> void:
+	# var is_connectable_string = "[Connectable] " if peripheral.is_connectable else "[Not connectable] "
+	# _terminal.write(prompt + ": ", Color.PURPLE)
+	# _terminal.write(peripheral.identifier + " " + str(peripheral.rssi) + "dBm " + is_connectable_string + " ")
+	# if data != "":
+	# 	_terminal.write(peripheral.address, Color.GREEN)
+	# 	_terminal.writeln(" => " + data, Color.TOMATO)
+	# else:
+	# 	_terminal.writeln(peripheral.address, Color.GREEN)
+	pass
 
 
 ## HMI callbacks
@@ -57,73 +60,81 @@ func _print_peripheral(peripheral : BLEPeripheral, prompt : String, data : Strin
 
 # On scan button pressed
 func _on_scan_pressed() -> void:
-	if !_ble_adapter.scan_is_active():
-		_ble_adapter.scan_start()
-	else:
-		_ble_adapter.scan_stop()
+	# if !_ble_adapter.scan_is_active():
+	# 	_ble_adapter.scan_start()
+	# else:
+	# 	_ble_adapter.scan_stop()
+	pass
 
 
 # On connect button pressed
 func _on_connect_pressed() -> void:
-	var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
-	if peripheral == null:
-		_terminal.writeln("Peripheral not found, please try to scan first...", Color.RED)
-	else:
-		if peripheral.is_connected:
-			peripheral.disconnect_peripheral()
-		else:
-			peripheral.connect_peripheral()
+	# var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
+	# if peripheral == null:
+	# 	_terminal.writeln("Peripheral not found, please try to scan first...", Color.RED)
+	# else:
+	# 	if peripheral.is_connected:
+	# 		peripheral.disconnect_peripheral()
+	# 	else:
+	# 		peripheral.connect_peripheral()
+	pass
 
 
 # On read button pressed
 func _on_read_pressed() -> void:
-	var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
-	var return_value = peripheral.read(_service.text, _characteristic.text)
-	if return_value != null:
-		var bytes : PackedByteArray = return_value
-		_print_peripheral(peripheral, "Read", bytes.get_string_from_utf8())
+	# var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
+	# var return_value = peripheral.read(_service.text, _characteristic.text)
+	# if return_value != null:
+	# 	var bytes : PackedByteArray = return_value
+	# 	_print_peripheral(peripheral, "Read", bytes.get_string_from_utf8())
+	pass
 
 
 # On send button pressed
 func _on_send_pressed() -> void:
-	var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
-	if peripheral.write_request(_service.text, _characteristic.text, _data.text):
-		_print_peripheral(peripheral, "Write", _data.text)
+	# var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
+	# if peripheral.write_request(_service.text, _characteristic.text, _data.text):
+	# 	_print_peripheral(peripheral, "Write", _data.text)
+	pass
 
 
 # On infos button pressed
 func _on_infos_pressed() -> void:
-	var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
-	if peripheral == null:
-		_terminal.writeln("Peripheral not found, please try to scan first...", Color.RED)
-	else:
-		for data in peripheral.manufacturer_data:
-			var bytes : PackedByteArray = peripheral.manufacturer_data[data];
-			_print_line("Manufacturer data", BLEUtils.get_company_name(data) + " (0x" + bytes.hex_encode() + ")")
-		for service in peripheral.services:
-			_print_line("Service", service)
-			for caracteristic in peripheral.services[service]:
-				_print_line("	Caracteristic", caracteristic)
-				for descriptor in peripheral.services[service][caracteristic]:
-					_print_line("		Descriptor", descriptor)
+	# var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
+	# if peripheral == null:
+	# 	_terminal.writeln("Peripheral not found, please try to scan first...", Color.RED)
+	# else:
+	# 	for data in peripheral.manufacturer_data:
+	# 		var bytes : PackedByteArray = peripheral.manufacturer_data[data];
+	# 		_print_line("Manufacturer data", BLEUtils.get_company_name(data) + " (0x" + bytes.hex_encode() + ")")
+	# 	for service in peripheral.services:
+	# 		_print_line("Service", service)
+	# 		for caracteristic in peripheral.services[service]:
+	# 			_print_line("	Caracteristic", caracteristic)
+	# 			for descriptor in peripheral.services[service][caracteristic]:
+	# 				_print_line("		Descriptor", descriptor)
+	pass
 
 
 # On notify button toggled
 func _on_notify_toggled(button_pressed : bool) -> void:
-	var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
-	if button_pressed:
-		peripheral.notify(_service.text, _characteristic.text)
-	else:
-		peripheral.unsubscribe(_service.text, _characteristic.text)
+	# var peripheral : BLEPeripheral = _ble_adapter.get_peripheral(_peripheral.text)
+	# if button_pressed:
+	# 	peripheral.notify(_service.text, _characteristic.text)
+	# else:
+	# 	peripheral.unsubscribe(_service.text, _characteristic.text)
+	pass
 
 
 ## BLE Adapter callbacks
 
 
 # On peripheral found
-func _on_ble_adapter_peripheral_found(peripheral : BLEPeripheral) -> void:
-	if (!peripheral.has_no_identifier and peripheral.is_connectable) or _advance.button_pressed:
-		_print_peripheral(peripheral, "Found")
+# func _on_ble_adapter_peripheral_found(peripheral : BLEPeripheral) -> void:
+func _on_ble_adapter_peripheral_found() -> void:
+	# if (!peripheral.has_no_identifier and peripheral.is_connectable) or _advance.button_pressed:
+	#	 _print_peripheral(peripheral, "Found")
+	pass
 
 
 # On scan start
@@ -137,46 +148,60 @@ func _on_ble_adapter_scan_stopped() -> void:
 
 
 # On peripheral indicate
-func _on_ble_adapter_peripheral_indicated(peripheral : BLEPeripheral, payload : PackedByteArray) -> void:
-	_print_peripheral(peripheral, "Indicate", payload.get_string_from_utf8())
+# func _on_ble_adapter_peripheral_indicated(peripheral : BLEPeripheral, payload : PackedByteArray) -> void:
+func _on_ble_adapter_peripheral_indicated(payload : PackedByteArray) -> void:
+	# _print_peripheral(peripheral, "Indicate", payload.get_string_from_utf8())
+	pass
 
 
 # On peripheral notify
-func _on_ble_adapter_peripheral_notified(peripheral : BLEPeripheral, payload : PackedByteArray) -> void:
-	_print_peripheral(peripheral, "Notify", payload.get_string_from_utf8())
+# func _on_ble_adapter_peripheral_notified(peripheral : BLEPeripheral, payload : PackedByteArray) -> void:
+func _on_ble_adapter_peripheral_notified(payload : PackedByteArray) -> void:
+	# _print_peripheral(peripheral, "Notify", payload.get_string_from_utf8())
+	pass
 
 
 # On peripheral connected
-func _on_ble_adapter_peripheral_connected(peripheral : BLEPeripheral) -> void:
-	if (!peripheral.has_no_identifier and peripheral.is_connectable) or _advance.button_pressed:
-		_print_peripheral(peripheral, "Connected")
-		_connect.text = "Disconnect"
-		_notify.disabled = false
-		_read.disabled = false
-		_send.disabled = false
+# func _on_ble_adapter_peripheral_connected(peripheral : BLEPeripheral) -> void:
+func _on_ble_adapter_peripheral_connected() -> void:
+	# if (!peripheral.has_no_identifier and peripheral.is_connectable) or _advance.button_pressed:
+	# 	_print_peripheral(peripheral, "Connected")
+	# 	_connect.text = "Disconnect"
+	# 	_notify.disabled = false
+	# 	_read.disabled = false
+	# 	_send.disabled = false
+	pass
 
 
 # On peripheral disconnected
-func _on_ble_adapter_peripheral_disconnected(peripheral : BLEPeripheral) -> void:
-	_print_peripheral(peripheral, "Disconnected")
-	_connect.text = "Connect"
-	_notify.disabled = true
-	_read.disabled = true
-	_send.disabled = true
+# func _on_ble_adapter_peripheral_disconnected(peripheral : BLEPeripheral) -> void:
+func _on_ble_adapter_peripheral_disconnected() -> void:
+	# _print_peripheral(peripheral, "Disconnected")
+	# _connect.text = "Connect"
+	# _notify.disabled = true
+	# _read.disabled = true
+	# _send.disabled = true
+	pass
 
 
 # On peripheral udpated
-func _on_ble_adapter_peripheral_updated(peripheral : BLEPeripheral) -> void:
-	if _update.button_pressed and ((!peripheral.has_no_identifier and peripheral.is_connectable) or _advance.button_pressed):
-		_print_peripheral(peripheral, "Update")
+# func _on_ble_adapter_peripheral_updated(peripheral : BLEPeripheral) -> void:
+func _on_ble_adapter_peripheral_updated() -> void:
+	# if _update.button_pressed and ((!peripheral.has_no_identifier and peripheral.is_connectable) or _advance.button_pressed):
+	# 	_print_peripheral(peripheral, "Update")
+	pass
 
 
 # On peripheral error status updated
-func _on_ble_adapter_peripheral_error_status_updated(peripheral : BLEPeripheral, code : int, ___ : String) -> void:
-	_print_peripheral(peripheral, "Error", BLEUtils.get_code_string(code))
+# func _on_ble_adapter_peripheral_error_status_updated(peripheral : BLEPeripheral, code : int, ___ : String) -> void:
+func _on_ble_adapter_peripheral_error_status_updated(code : int, ___ : String) -> void:
+	# _print_peripheral(peripheral, "Error", BLEUtils.get_code_string(code))
+	pass
 
 
 # On adapter error status updated
+# func _on_ble_adapter_adapter_error_status_updated(code : int, __ : String) -> void:
 func _on_ble_adapter_adapter_error_status_updated(code : int, __ : String) -> void:
-	_terminal.writeln("Adapter " + " (" + BLEUtils.get_code_string(code) + ") ", Color.RED)
+	# _terminal.writeln("Adapter " + " (" + BLEUtils.get_code_string(code) + ") ", Color.RED)
+	pass
 
